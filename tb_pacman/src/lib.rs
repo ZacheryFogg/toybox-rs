602613f831cb323dbb9e8de8161d7bb177570a3e
Zachery Fogg <z.fogg1234@gmail.com>
2021-04-05 10:18:59 -0400

    Started Development of Pacman environment

---
+++ tb_pacman/src/lib.rs
@@ -0,0 +1,15 @@
+extern crate serde;
+extern crate serde_json;
+extern crate toybox_core;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate schemars;
+extern crate rand;
+
+pub mod amidar;
+mod digit_sprites;
+mod types;
+
+pub use crate::types::Amidar as Pacman;
+pub use crate::types::State;
